3eb3d4cc8b92899d5acbb5d04bc7b3a1caafa19a
valminjr <junior.valmir6@gmail.com>
2019-08-29 21:51:17 -0300

    update: reactotron-redux configuration

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import "./config/ReactotronConfig";
 
 import GlobalStyles from "./styles/global";
 import Header from "./components/Header/index";
 import Routes from "./routes";
 
+import store from "./store";
+
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
